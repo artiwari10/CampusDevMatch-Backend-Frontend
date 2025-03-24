965427c2f61f63c7e96b536ebe6e4d99557412ef
Anmol Ratan TIwari <66309398+artiwari10@users.noreply.github.com>
2025-03-24 15:59:30 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
         { withCredentials: true }
       );
       dispatch(addUser(res.data.data));
-      return navigate("/profile");
+      return navigate("/feed");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong");
     }
