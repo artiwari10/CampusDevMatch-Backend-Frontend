202142a90486c5c00cf35195bb9f3623728efc3f
Anmol Ratan TIwari <66309398+artiwari10@users.noreply.github.com>
2025-04-29 15:39:09 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const intializeSocket = require("./utils/socket");
 require('dotenv').config();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://campus-dev-match-backend-frontend.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE']
 }));
